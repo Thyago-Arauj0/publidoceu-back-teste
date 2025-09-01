16154015497a0bedee9c61afc021dc8b79ad2b3f
Thyago <thiagogg020676@gmail.com>
2025-09-01 13:45:04 -0300

    config

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
-
-from drf_spectacular.views import SpectacularRedocView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
+    path("api/v1/", include("api.urls")),
 
-    path('api/v1/', include('api.urls')),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    
+    # rota do schema em JSON
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+
+    # documentação Swagger
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+
+    # documentação Redoc
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 
 if settings.DEBUG:
-
     urlpatterns.append(path("admin/", admin.site.urls))
