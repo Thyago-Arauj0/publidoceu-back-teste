08f3737c916531ad45e283280e8aabbfc6794120
Thyago <thiagogg020676@gmail.com>
2025-09-30 14:25:55 -0300

    fix: checklist views

--- api/checklist/views.py
+++ api/checklist/views.py
@@ -5,33 +5,20 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Checklist
 from .serializers import ChecklistSerializer
 
-from rest_framework.permissions import (
-
-    IsAdminUser
-
-)
+from rest_framework.permissions import IsAuthenticated
 
 from api.card.models import Card
 
 class ChecklistViewSet(ModelViewSet):
 
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated]
     serializer_class = ChecklistSerializer
 
     def get_queryset(self):
-
         card_id = self.kwargs.get('card_pk')
-        user = self.request.user
-
         if card_id is None:
-
             return Checklist.objects.none()
-
-        if user.is_staff or user.is_superuser:
-
-            return Checklist.objects.filter(card_id=card_id).order_by('-id')
-
-        return Checklist.objects.none()
+        return Checklist.objects.filter(card_id=card_id).order_by('-id')
 
 
 
